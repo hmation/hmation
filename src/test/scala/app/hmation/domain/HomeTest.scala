14dee1d980ce0553fde1acd82d7a6921912ef5c3
Greg Maslowski <grzegorz.maslowski@ista.pl>
2018-11-02 08:03:10 +0100

    Switch to inmem in test; removed UUIDs.

--- src/test/scala/app/hmation/domain/HomeTest.scala
+++ src/test/scala/app/hmation/domain/HomeTest.scala
@@ -1,10 +1,9 @@
 package app.hmation.domain
 
-import java.util.UUID.randomUUID
-
 import akka.testkit.TestKit
 import app.hmation.domain.Home.Commands.{AddDevice, GetDevice}
 import app.hmation.test.common.AkkaActorTest
+import com.softwaremill.id.pretty.PrettyIdGenerator
 
 class HomeTest extends AkkaActorTest {
 
@@ -15,7 +14,7 @@ class HomeTest extends AkkaActorTest {
   }
 
   "Home" should {
-    val homeEntity = system.actorOf(Home.props(randomUUID().toString))
+    val homeEntity = system.actorOf(Home.props(PrettyIdGenerator.singleNode.nextId()))
 
     "store a device and be able to get it out" in {
 
