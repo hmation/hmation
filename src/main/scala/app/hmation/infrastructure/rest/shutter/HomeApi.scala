55c1eaea6595372eb0ed317edf1f8b4eb1fb5488
Greg Maslowski <grzegorz.maslowski@ista.pl>
2018-11-02 08:18:02 +0100

    Fixes and resolves #19 #20 #21 #22 #23

--- src/main/scala/app/hmation/infrastructure/rest/shutter/HomeApi.scala
+++ src/main/scala/app/hmation/infrastructure/rest/shutter/HomeApi.scala
@@ -1,13 +1,12 @@
 package app.hmation.infrastructure.rest.shutter
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import spray.json.{DefaultJsonProtocol, JsValue}
+import spray.json.DefaultJsonProtocol._
+import spray.json.JsValue
 
 final case class HomeDto(name: String, data: JsValue)
 
 trait HomeJsonSupport extends SprayJsonSupport {
-
-  import DefaultJsonProtocol._
-
+  ł
   implicit val homeDtoJsonFormat = jsonFormat2(HomeDto)
 }
